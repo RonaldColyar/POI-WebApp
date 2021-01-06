8e7ec1a210b98e86992b90c934416ec8c48bfdd4
RonaldColyar <PhantexCreations@gmail.com>
2021-01-06 16:48:32 -0600

    basic account bar finished

--- React-Poi/src/accountbar.js
+++ React-Poi/src/accountbar.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Accountbar() {
     return (
         <div id = "accountbar">
+            <button id = "logoutbutton"> Logout </button>
+            <button id = "contactsbutton">Contacts</button>
+            <button id = "breachbutton">Breach(Very Dangerous)</button>
             
         </div>
     )
