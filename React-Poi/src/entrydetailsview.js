b7a75ac15e2326bf1b0cbad4347eb48397e2fa4b
RonaldColyar <PhantexCreations@gmail.com>
2021-01-06 14:13:18 -0600

    finished entry details styling

--- React-Poi/src/entrydetailsview.js
+++ React-Poi/src/entrydetailsview.js
@@ -10,9 +10,13 @@ export default function DetailsView() {
                 <div id ="details_level_img_wrapper" >
                     <img id= "details_level_img" src = "../Images/level1.png"></img>
                 </div>
-                <h3>Label</h3>
-                <p>Details</p>
-                <h3>Date</h3>
+                <h3 id = "entrynickname" >Label</h3>
+                <h3 id = "entry_date">Date</h3>
+                <h3 id = "threat_level">Threat Level</h3>
+                <div id = "entry_details_wrapper">
+                    <p id = "entry_details">Details</p>
+                </div>
+                
                     
                
             </div>
