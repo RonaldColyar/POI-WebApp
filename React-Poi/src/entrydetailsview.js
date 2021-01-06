c51fc7522d7ad3110f34af9bb4772df7eb494b1b
RonaldColyar <PhantexCreations@gmail.com>
2021-01-06 00:40:46 -0600

    general idea for entry details

--- React-Poi/src/entrydetailsview.js
+++ React-Poi/src/entrydetailsview.js
@@ -6,8 +6,15 @@ export default function DetailsView() {
             <div id = "DetailedHeader">
                 <h3>Entry Details</h3>
             </div>
-            <div>
-                
+            <div id= "ActualDetails">
+                <div id ="details_level_img_wrapper" >
+                    <img id= "details_level_img" src = "../Images/level1.png"></img>
+                </div>
+                <h3>Label</h3>
+                <p>Details</p>
+                <h3>Date</h3>
+                    
+               
             </div>
             
         </div>
