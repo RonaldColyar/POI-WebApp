8ab15050fa03c740468fe50f925c3a631c854875
RonaldColyar <PhantexCreations@gmail.com>
2021-01-12 00:56:29 -0600

    restful server configuration

--- Server/Server.js
+++ Server/Server.js
@@ -1,3 +1,6 @@
+const { timingSafeEqual } = require("crypto");
+const e = require("express");
+const { func } = require("prop-types");
 
 
 function EmailManager(){
@@ -5,30 +8,19 @@ function EmailManager(){
 }
 
 
-/* 
-ContactsManager handles the CRUD of the  contact list of the user
-contactsManager has a component EmailManager that contribute
-to its breach protocol capabilites.
 
-*/
 
-function ContactsManager(collection){
-    
-
-}
 
 /*
 Mongo Manager handles all of the interaction with 
-the mongo database. Mongo Manager is composed of smaller
-classes that handles specific functionality for optimal
-encapsulation.
+the mongo database.The update query could be polymorphed, but for
+the sake of readability its repeated.
 
 */
 function MongoManager(){
     this.uri = "mongodb://localhost:27017/";
     this.mongo = require("mongodb").MongoClient;
     this.connected = false;
-    
     const self = this;
 
     //makes the user collection accessible by other class methods
@@ -40,7 +32,6 @@ function MongoManager(){
             else{
                 const db = client.db("POI");
                 self.collection = db.collection("users");
-                this.contacts_manager = new ContactsManager(self.collection);
                 self.connected = true;
             }
         })
@@ -77,19 +68,14 @@ function MongoManager(){
 
     this.update_and_check = function(wherequery,setquery,response){
         this.collection.updateOne(wherequery ,setquery , function(error,result){
-            if(error){
-                response.json({status:"error"});
-            }
-            else{
-                response.json({status:"success"});
-            }
+            this.CUD_error_check(error,response);
         } );
 
     }
-    this.create_person = function(email,data,response){
+    this.create_person = function(data,response){
         if(this.connected == true){
             const name = data.first + data.last
-            this.update_and_check({email :email},{$set:{persons: {[name]:""}}},response);
+            this.update_and_check({email :data.email},{$set:{persons: {[name]:" "}}},response);
         }
         else{
             response.json({status:"error"});
@@ -97,10 +83,10 @@ function MongoManager(){
     }
 
     //repeated logic as create_person, but for understanding purposes
-    this.create_entry = function(email,data,response){
+    this.create_entry = function(data,response){
         if(this.connected == true){
             const name = data.first +data.last
-            this.update_and_check({email :email} , {
+            this.update_and_check({email :data.email} , {
                 $set:{persons :{[name] : 
                     {entries : 
                         {[data.label] :
@@ -126,6 +112,13 @@ function MongoManager(){
             });
         }
     }
+    this.add_contact = function(email,contacts_email,response){
+        this.collection.updateOne({email:email} 
+            , {$set :{contacts:{[contacts_email]:" "}}},function(error,result){
+               this.CUD_error_check(error,response)
+            })
+}
+
     this.user_exist = function(email){
         var user_exists = false;
         if (this.connected == true){
@@ -139,7 +132,36 @@ function MongoManager(){
         return user_exists;
 
     }
-    
+    //breach of security
+    this.delete_all = function(email,response){
+        this.collection.deleteMany({email:email} , function(error,result){
+            this.CUD_error_check(error,response);
+
+        })
+
+    }
+    //delete person path : const child_to_remove = "persons." +name;
+    //delete person query: {$set :{[child_to_remove] : ""}
+
+    //general purpose removal of a child
+    this.delete = function(email,name,response,query){
+        
+        this.collection.updateOne({email:email} ,query 
+        , function(error,result){
+            this.CUD_error_check(error,response)
+        })
+    }
+
+    this.gather_all = function(email){
+        var data;
+        this.collection.findOne({email:email} , function(error,result){
+            if(typeof error === 'undefined'){
+              data = result;
+            }
+           
+        })
+        return data;
+    }
 
 }
  
@@ -155,6 +177,7 @@ function SessionManager(){
         else{
             const token = uuidv4();
             this.sessions.set(email,token);
+            response.json({status:"AUTHED" , auth_token :token});
 
         }
     }
@@ -170,7 +193,18 @@ function SessionManager(){
 
 
     }
-    this.import_locked_accounts = function(){
+    this.is_authed = function(email,token){
+        var auth_status = false;
+        if(this.sessions.has(email)){
+            // if the user has the auth token generated for the session
+            if(this.sessions.get(email) == token){
+                auth_status = true;
+            }
+
+        }
+        return auth_status;
+    }
+    this.add_locked_accounts = function(accounts){
 
     }
     
@@ -180,9 +214,75 @@ function ServerRequestHandler (){
      this.express = require("express");
      this.app = this.express();
      this.fs = require("fs");
-     app.listen(8020); // start listening for requests
      this.mongo_manager = new MongoManager();
      this.session_manager = new SessionManager();
+     var self = this;
+     this.check_auth_and_proceed = function(res,request,response){
+        if(self.session_manager.is_authed(request.body.email,request.body.token) == true){
+            res()
+        }
+        else{
+            response.json({status:"error"})
+        }
+     }
+     
+
+     this.app.post("/signup" , function(request,response){
+         if(self.mongo_manager.user_exist(request.body.email) == true){
+             response.json({status:"error"})
+         }
+         else{
+             self.mongo_manager.create_user(request.body)
+         }
+     })
+
+     this.app.post("/login", function(request,response){
+         if(self.mongo_manager.user_exist(request.body.email) == true){
+             //if the codes match
+             if(self.mongo_manager.user_code(request.body.email) == request.body.code){ 
+                 //add session and send authentication token
+                 self.session_manager.add_session(request.body.email,response)
+             }
+             else{//wrong code
+                response.json({status:"error"}) 
+             }
+         }
+        else{ //user does not exist
+            response.json({status:"error"})
+        }
+     })
+    this.app.post("/userprofiledata", function(request,response){
+        this.check_auth_and_proceed(function(){
+            response.json({status:"DATA" , data: self.mongo_manager.gather_all(request.body.email)})
+        },request,response)
+
+    })
+
+    this.app.post("/logout", function(request,response){
+        this.check_auth_and_proceed(function(){
+            self.session_manager.remove_session(request.body.email,response)
+        },request,response)
+
+    })
+    this.app.delete("/breached", function(request,response){
+        this.check_auth_and_proceed(function(){
+            self.mongo_manager.delete_all(email,response)
+        },request,response)
+    })
+    this.app.post("/addperson",function(request,response){
+        this.check_auth_and_proceed(function(){
+                self.mongo_manager.create_person(request.body,response)
+        })
+    })
+    this.app.put("/addentry",function(request,response){
+        this.check_auth_and_proceed(function(){
+            self.mongo_manager.create_entry(request.body,response)
+        })
+        
+    })
 
 
+
+
+     app.listen(8020);
 }
