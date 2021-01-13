266cb79923dbe09dd745a143ff461571e697bb08
RonaldColyar <PhantexCreations@gmail.com>
2021-01-13 03:22:47 -0600

    Client side resource fetching skeleton implementation

--- React-Poi/src/HubPage.js
+++ React-Poi/src/HubPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState,useRef} from 'react'
 import Accountbar from './accountbar'
 import Confirmbreachpopup from './Confirmbreachpopup'
 import ContactsPopup from './ContactsPopup'
@@ -11,23 +11,65 @@ import Profilesummary from './profilesummary'
 import Profileview from './Profileview'
 import StatsBar from './statsbar'
 
+
+
 export default function HubPage(params) {
-    console.log(params.match.params.email)
-    return (
-        <div id = "hub_main_wrapper">    
-            
-            <Profilesummary data = "insert data"/>
-            <Entryhubview data = "insert data"/>
-            <Profileview data = "insert data"/>
-            <DetailsView data = "insert data"/>
-            <StatsBar data = "insert data"/>
-            <Accountbar data = "insert data" />
-            <Newpersonpopup/>    
-            <Newentrypopup/>
-            <Confirmbreachpopup/>    
-            <ContactsPopup/>
-            <Createcontactspopup/>    
-        </div>
-            
-    )
+    const [person , changeperson] = useState([])
+    this.email = params.match.params.email;
+    this.token = localStorage.getItem("poitoken");
+
+    this.fetch_and_respond =async function(header,url){
+        const response = await  fetch(url,header)
+        const data = await response.json()
+        return data  
+    }
+
+    this.profile_data = async function(){
+        const url = "http://localhost:8020/userprofiledata/" + this.email+"/"+this.token
+        const header = {method:"GET",body: {}} ;
+        return await  this.fetch_and_respond(header,url)
+    }
+
+    this.create_new_person = async function(data){
+        const url = "http://localhost:8020/addperson";
+        data.token = this.token;
+        data.email = this.email;
+
+        const header = {
+            method : "POST",
+            body : data
+        };
+        return await this.fetch_and_respond(header,url);
+    
+    }
+    this.breach = async function(){
+        
+    }
+    //not authed or server issues
+    if(this.profile_data.status == "error"){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+    else{
+        return (
+        
+            <div id = "hub_main_wrapper">    
+                <Profilesummary data = {person}/>
+                <Entryhubview data = {person}/>
+                <Profileview persons = {this.profile_data} selector = {changeperson}/>
+                <DetailsView data = {person}/>
+                <StatsBar />
+                <Accountbar data = {this.email} />
+                <Newpersonpopup/>    
+                <Newentrypopup/>
+                <Confirmbreachpopup/>    
+                <ContactsPopup/>
+                <Createcontactspopup/>    
+            </div>
+                
+        )
+    }
 }
