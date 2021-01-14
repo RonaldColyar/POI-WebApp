d24fec883844fcdfb34e1ca60427c1dc3ea96070
RonaldColyar <PhantexCreations@gmail.com>
2021-01-14 00:13:45 -0600

    Added new component states

--- React-Poi/src/accountbar.js
+++ React-Poi/src/accountbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function Accountbar() {
+export default function Accountbar({actions}) {
+    var self = this;
+    this.check_logout_status = async function(){
+        response = await actions.logout()
+        if(response.status == "error"){
+            //popup
+        }
+        else{
+            //popup
+        }   
+    }
+
+    
     return (
         <div id = "accountbar">
-            <button id = "logoutbutton"> Logout </button>
+            <button id = "logoutbutton" onClick = {self.check_logout_status()}> Logout </button>
             <button id = "contactsbutton">Contacts</button>
             <button id = "breachbutton">Breach(Very Dangerous)</button>
             
